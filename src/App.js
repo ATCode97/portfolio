cd8e3296e74461dae778cb42a98f7b7214150fb2
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-09-27 17:46:19 +0100

    added files to asserts for design and also made navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import MyNavbar from "./components/MyNavbar";
 
 const App = () => {
-  return <div className="App">my portfolio</div>;
+  return (
+    <div className="App">
+      <MyNavbar />
+      my portfolio
+    </div>
+  );
 };
 
 export default App;
