f06ead362e3879389fde0eeb5c2436623a717727
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-09-28 15:03:32 +0100

    made landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
-import MyNavbar from "./components/MyNavbar";
+import MyNavbar from "./components/Navbar/MyNavbar";
+import MyCarousel from "./components/Carousel/MyCarousel";
+import TitleMessage from "./components/TitleMessage/TitleMessage";
 
 const App = () => {
   return (
     <div className="App">
       <MyNavbar />
-      my portfolio
+      <MyCarousel />
+      <TitleMessage />
     </div>
   );
 };
