3838f94e6ab934dc06eab76c4d015f16c8ad13ea
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-09-29 02:30:09 +0100

    created and added aboutme page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { Fade } from "react-reveal";
+import { Parallax } from "react-parallax";
+import { Container } from "react-bootstrap";
+
 import "./App.css";
 import MyNavbar from "./components/Navbar/MyNavbar";
 import MyCarousel from "./components/Carousel/MyCarousel";
 import TitleMessage from "./components/TitleMessage/TitleMessage";
+import AboutMe from "./pages/About/AboutMe";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App" style={{ position: "relative" }}>
       <MyNavbar />
       <MyCarousel />
       <TitleMessage />
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <Container className="container-box rounded">
+            <Fade duration={500}>
+              <AboutMe />
+            </Fade>
+          </Container>
+        </Parallax>
+      </div>
     </div>
   );
 };
---
+++ src/pages/About/AboutMe.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Container, Col, Row, Image, Button } from "react-bootstrap";
+import Profile from "../../assets/img/profile/profile.webp";
+import "./aboutme.css";
+//xs and md are the two individual col
+
+const AboutMe = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">About Me</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2">
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                  thumbnail
+                  fluid
+                />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi! I am Alistair Tsang
+                <br />
+                <br /> A Recent graduate from NorthCoder bootcamp. I have
+                experience in JavaScript, Node.js, HTML and CSS. Also have
+                experience using Express, Axios, Knex and PSQL to build backend
+                servers.
+                <br />
+                In 2018 I graduated at Hull University reading History and
+                Politic
+                <br />
+                <br /> <br />
+              </Row>
+              <Row>
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        {" "}
+                        Let's Talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://drive.google.com/file/d/1W45yU2OLuNmNQ-zjyfEITbRVGvo1KS8R/view?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/ATCode97"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://www.linkedin.com/in/alistair-tsang-2ba049171/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
