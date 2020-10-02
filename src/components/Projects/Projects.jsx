4361ecd52c53bb8f8b9ebd75741074a7a09b71ee
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-10-02 19:38:44 +0100

    added to project page and change NC backend project image

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,6 +6,8 @@ import L_NcNewsBE from "../../assets/img/projects/NC_NewsBE.webp";
 import L_NcNewsFE from "../../assets/img/projects/NC_NewsFE.webp";
 import L_TechReview from "../../assets/img/projects/Tech_Review.webp";
 import L_worldArt from "../../assets/img/projects/worldArt.webp";
+import L_WordComparison from "../../assets/img/projects/word_comparison.webp";
+import L_WCBE from "../../assets/img/projects/word_comparison_be.webp";
 
 import L_AWS_Api from "../../assets/img/projects/aws-api-gateway.svg";
 import L_DynamoDb from "../../assets/img/projects/aws-dynamodb.svg";
@@ -20,6 +22,8 @@ import L_POSTGRESQL from "../../assets/img/skills/postgresql.svg";
 import L_CSS3 from "../../assets/img/skills/css3.svg";
 import L_BOOTSTRAP4 from "../../assets/img/skills/bootstrap-4.svg";
 import L_GIT from "../../assets/img/skills/github-api.svg";
+import L_MOCHA from "../../assets/img/skills/mocha.svg";
+import L_CHAI from "../../assets/img/skills/chai.svg";
 
 import "./projects.css";
 
@@ -58,6 +62,7 @@ const Projects = () => {
                         <hr />
                         <strong>Features:</strong>
                         <ul className="list-styles pt-1">
+                          <li>All routes are tested</li>
                           <li>RESTful API</li>
                           <li>Can get articles and comments</li>
                           <li>Post comments</li>
@@ -103,6 +108,28 @@ const Projects = () => {
                           <li>
                             <span className="p-2">
                               <Image
+                                src={L_MOCHA}
+                                alt="Mocha"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Mocha
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CHAI}
+                                alt="Chai"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Chai
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
                                 src={L_GIT}
                                 alt="Github"
                                 rounded
@@ -119,7 +146,7 @@ const Projects = () => {
               </div>
               <div className="d-flex justify-content-between flex-nowrap text-center">
                 <UrlButton
-                  href="https://github.com/akjha96/getGithubInfo"
+                  href="https://northcodersnews2020.herokuapp.com/api"
                   target="_blank"
                 >
                   SOURCE CODE
@@ -463,6 +490,227 @@ const Projects = () => {
               <div className="d-flex justify-content-between flex-nowrap text-center">
                 <UrlButton
                   href="https://github.com/keirbetts/tech-review"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          <ImageEvent
+            date="September, 2020"
+            className="text-center"
+            text="Word Comparison Backend API"
+            src={L_WCBE}
+            alt="Word Comparison Backend"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> An express server built
+                        with a RESTful architectural style using SQL database
+                        along with Postgres and knex for various endpoints. This
+                        hosted database is use to interact with data that would
+                        be use for my front end nc news projects.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>All routes are tested</li>
+                          <li>RESTful API</li>
+                          <li>Can get comparison information</li>
+                          <li>Post Comparison to PSQL table</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_NODE_JS}
+                                alt="NodeJS"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              NodeJS
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_EXPRESS}
+                                alt="Express"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              Express
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_POSTGRESQL}
+                                alt="PostgreSQL"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              PostgreSQL
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_MOCHA}
+                                alt="Mocha"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Mocha
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CHAI}
+                                alt="Chai"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Chai
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_GIT}
+                                alt="Github"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Git Version control
+                            </span>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/ATCode97/word_comparisonBE"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+                <UrlButton
+                  href="https://wordcomparisonbe.herokuapp.com/api"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          <ImageEvent
+            date="September, 2020"
+            className="text-center"
+            text="Word Comparison"
+            src={L_WordComparison}
+            alt="Word Comparison"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> An React web app thats
+                        will take two words that will be compare to each other
+                        and will provide true or false if words are either an an
+                        anagram of another or whether the word is a palindrome
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>Real-time API calls</li>
+                          <li>Does post requests</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_NODE_JS}
+                                alt="NodeJS"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              NodeJS
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="ReactJS"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              ReactJS
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_BOOTSTRAP4}
+                                alt="Bootstrap 4"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Bootstrap 4
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_GIT}
+                                alt="Github"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Git Version control
+                            </span>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://github.com/ATCode97/fe-word-comparison"
                   target="_blank"
                 >
                   SOURCE CODE
